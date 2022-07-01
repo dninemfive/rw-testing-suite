8f0da0fa355992511017de30560c71f459bf60ac
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-07-01 16:15:46 -0700

    switch to using base game LogMessageType

--- Source/Tests.cs
+++ Source/Tests.cs
@@ -7,33 +7,32 @@ using Verse;
 
 namespace RimworldTestingSuite
 {
-    public enum AssertionLevel { Message, Warning, Error }
     public static class Tests
     {
-        private static Dictionary<AssertionLevel, Action<string>> Console = new Dictionary<AssertionLevel, Action<string>>()
+        private static Dictionary<LogMessageType, Action<string>> Console = new Dictionary<LogMessageType, Action<string>>()
         {
-            { AssertionLevel.Message, (string s) => Log.Message(s) },
-            { AssertionLevel.Warning, (string s) => Log.Warning(s) },
-            { AssertionLevel.Error,   (string s) => Log.Error(s)   }
+            { LogMessageType.Message, (string s) => Log.Message(s) },
+            { LogMessageType.Warning, (string s) => Log.Warning(s) },
+            { LogMessageType.Error,   (string s) => Log.Error(s)   }
         };
-        public static void Assert<T>(T operand, Func<T, bool> assertion, AssertionLevel level = AssertionLevel.Warning)
+        public static void Assert<T>(T operand, Func<T, bool> assertion, LogMessageType level = LogMessageType.Warning)
         {
             bool result = assertion(operand);
             // TODO: use attribute name
             string resultDescription = result ? $"Assertion successful" : "Assertion failed";
             Console[level](resultDescription);
         }
-        public static void AssertTrue(bool assertion, AssertionLevel level = AssertionLevel.Warning)
+        public static void AssertTrue(bool assertion, LogMessageType level = LogMessageType.Warning)
             => Assert(assertion, (bool b) => assertion == true, level);
-        public static void AssertFalse(bool assertion, AssertionLevel level = AssertionLevel.Warning)
+        public static void AssertFalse(bool assertion, LogMessageType level = LogMessageType.Warning)
             => Assert(assertion, (bool b) => assertion == false, level);
-        public static void AssertEqual(object operand, object target, AssertionLevel level = AssertionLevel.Warning)
+        public static void AssertEqual(object operand, object target, LogMessageType level = LogMessageType.Warning)
             => Assert(operand, (object o) => o == target, level);
-        public static void AssertNotEqual(object operand, object target, AssertionLevel level = AssertionLevel.Warning)
+        public static void AssertNotEqual(object operand, object target, LogMessageType level = LogMessageType.Warning)
             => Assert(operand, (object o) => o != target, level);
-        public static void AssertNull(object operand, AssertionLevel level = AssertionLevel.Warning)
+        public static void AssertNull(object operand, LogMessageType level = LogMessageType.Warning)
             => Assert(operand, (object o) => o is null, level);
-        public static void AssertNonNull(object operand, AssertionLevel level = AssertionLevel.Warning)
+        public static void AssertNonNull(object operand, LogMessageType level = LogMessageType.Warning)
             // i miss C# 9 already ;-;
             => Assert(operand, (object o) => !(o is null), level);
     }

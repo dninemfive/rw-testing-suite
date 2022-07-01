075729afa391e2c9e6014e81932f1da9c089196f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-07-01 16:42:21 -0700

    sketch bootstrapper

---
+++ Source/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimworldTestingSuite
+{
+    // TODO: way to have tests run at other times (e.g. on main menu display, in-game)
+    [StaticConstructorOnStartup]
+    static class Bootstrapper
+    {
+        static Bootstrapper()
+        {
+            // foreach method with TestAttribute,
+            //  print "{type TestAttribute name ?? type name}.{method TestAttribute name} tests:"
+            //  run method (should be IEnumerable<bool>)
+            //  print number of successful tests out of number of total tests
+        }
+    }
+}
